9a19f2f29062b41c715f12def09ca0fd9d16da1c
Miguel Stein Martins <93017209+MigasStein@users.noreply.github.com>
2026-04-23 14:45:10 -0300

    Add files via upload

---
+++ calculadora.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.print("Digite o primeiro numero: ");
+		double priNumero = teclado.nextDouble();
+
+		System.out.print("Digite o operador (+, -, *, /): ");
+		char op = teclado.next().charAt(0);
+
+		System.out.print("Digite o segundo número: ");
+		double segNumero = teclado.nextDouble();
+
+		double resultado;
+
+		switch (op) {
+		case '+':
+			resultado = priNumero + segNumero;
+			break;
+		case '-':
+			resultado = priNumero - segNumero;
+			break;
+		case '*':
+			resultado = priNumero * segNumero;
+			break;
+		case '/':
+			if (segNumero == 0) {
+				System.out.println("Erro: divisão por zero!");
+				return;
+			}
+			resultado = priNumero / segNumero;
+			break;
+		default:
+			System.out.println("Operador inválido!");
+			return;
+		}
+
+		System.out.println("Resultado: " + resultado);
+		teclado.close();
+
+	}
+
+}
